5d0affec639055bffcf2f2655b04af96959acfa3
Charl de Waal <charldewaal8@gmail.com>
2022-07-17 16:21:14 +0200

    Removed paragraph

--- pages/index.js
+++ pages/index.js
@@ -22,10 +22,6 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Hello, I'm Charl. I'm a software developer and freesurfer from South Africa</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
       </section>
 
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
